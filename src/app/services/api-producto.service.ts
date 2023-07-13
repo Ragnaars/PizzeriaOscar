af5b8265dceccfb1e708fc38f024280721763fd5
Orlando Jafet Tobar Diaz <orl.tobar@duocuc.cl>
2023-07-13 15:01:18 -0400

    Api en glitch funcionando.

--- src/app/services/api-producto.service.ts
+++ src/app/services/api-producto.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+import { Injectable, OnInit } from '@angular/core';
 import { ProductoConId, Producto, ProductoParcial } from "../interfaces/producto";
 import { HttpClient } from "@angular/common/http";
 import { Observable, BehaviorSubject } from "rxjs";
@@ -10,9 +10,11 @@ import { delay } from "rxjs/operators";
 })
 export class ApiProductoService {
 
+
+
   public stock: number = 0;
   private Page = 1;
-  private UrlApi = "http://localhost:3000/producto/";
+  private URL_PRODUCTOS = 'https://tortoiseshell-cooperative-taste.glitch.me/producto';
   private comLista = new BehaviorSubject<Array<ProductoConId>>([]);
   //CREAR OBSERVADOR
   public listaProducto$ = this.comLista.asObservable();
@@ -23,7 +25,7 @@ export class ApiProductoService {
   ) { }
 
   public obtenerPrimerosProductos() {
-    this.http.get<ProductoConId[]>(`${this.UrlApi}?_page=1`).pipe(
+    this.http.get<ProductoConId[]>(`${this.URL_PRODUCTOS}?_page=1`).pipe(
       delay(2000)
     )
       .subscribe(resp => {
@@ -31,8 +33,21 @@ export class ApiProductoService {
         this.comLista.next(resp);
       })
   }
+
+  public obtenerPrimerosProductos1() {
+    this.http.get<Array<ProductoConId>>(`${this.URL_PRODUCTOS}}`)
+      .pipe(
+        delay(2000)
+      )
+      .subscribe(resultado => {
+        this.Page = this.Page + 1;
+        this.comLista.next(resultado);
+      });
+  }
+
+
   public obtenerMasElementos() {
-    this.http.get<Array<ProductoConId>>(`${this.UrlApi}?_page=${this.Page}`).pipe(
+    this.http.get<Array<ProductoConId>>(`${this.URL_PRODUCTOS}?_page=${this.Page}`).pipe(
       delay(2000)
     ).subscribe(resp => {
       this.Page = this.Page + 1;
@@ -46,7 +61,7 @@ export class ApiProductoService {
     //     'Content-Type':'application/json; charset=utf-8'
     //   }
     // })
-    return this.http.post<ProductoConId>(this.UrlApi, producto, {
+    return this.http.post<ProductoConId>(this.URL_PRODUCTOS, producto, {
       headers: {
         'Content-Type': 'application/json; charset=utf-8'
       }
@@ -54,22 +69,22 @@ export class ApiProductoService {
   }
 
   public getProducto(id: number): Observable<ProductoConId | null> {
-    // const path = this.UrlApi + id;
-    return this.http.get<ProductoConId | null>(`${this.UrlApi}/${id}`)
+    // const path = this.URL_PRODUCTOS + id;
+    return this.http.get<ProductoConId | null>(`${this.URL_PRODUCTOS}/${id}`)
   }
 
   public eliminarPublicacion(id: number): Observable<any> {
-    // const path = this.UrlApi + id;
+    // const path = this.URL_PRODUCTOS + id;
     // return this.http.delete(path)
 
-    return this.http.delete(`${this.UrlApi}/${id}`);
+    return this.http.delete(`${this.URL_PRODUCTOS}/${id}`);
   }
 
   public actualizarPublicacion(producto: Producto, id: number) {
-    const path = this.UrlApi + id;
+    const path = this.URL_PRODUCTOS + id;
     return this.http.patch<Producto>(path, producto);
 
-    // return this.http.patch(`${this.UrlApi}/${id}`,producto,{
+    // return this.http.patch(`${this.URL_PRODUCTOS}/${id}`,producto,{
     //   headers: {
     //     'Content-Type':'application/json; charset=utf-8'
     //   }
