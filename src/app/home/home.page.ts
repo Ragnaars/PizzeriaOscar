11cbb2c1c9f377796406d8ba5cd7ab90e52d3db4
Orlando Jafet Tobar Diaz <orl.tobar@duocuc.cl>
2023-07-12 20:46:48 -0400

    Primer commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -6,7 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-
+  npizzas:number = 8;
+  countrycode:string="56";
+  whastappnumber:string="954890716";
+  url:string="http://wa.me/"+this.countrycode+this.whastappnumber+"?text='Hola, quisiera hacer un pedido de "+this.npizzas+" pizzas'. Este pedido fue realizado desde ionic";
   constructor() {}
 
 }
